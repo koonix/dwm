3fd04fe55e4874a1d878d7a5886e61a601c7ee2a
Ehsan G <ehsangn@protonmail.ch>
2021-11-14 17:30:14 +0330

    add a prompt to dmenu_run

--- config.h
+++ config.h
@@ -137,7 +137,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "Programs", NULL };
 static const char *terminal[] = { TERM, NULL };
 
 /* tui */
